54b6788efcb058212194a6b702ee3fe4b27dfb95
Dhananjay0908 <121677939+Dhananjay0908@users.noreply.github.com>
2026-08-01 17:02:59 +0530

    Update database connection string in hospital.c

--- hospital.c
+++ hospital.c
@@ -7,8 +7,8 @@ int main() {
     printf("Content-Type: application/json\n\n");
 
     // External Cloud Database Connection String (Supabase/Neon Pooler URL)
-    const char *conninfo = "postgresql://postgres.xyzproject:password@aws-0-ap-south-1.pooler.supabase.com:6543/postgres";
-
+   // const char *conninfo = "postgresql://postgres.xyzproject:password@aws-0-ap-south-1.pooler.supabase.com:6543/postgres";
+    const char *conninfo = "postgresql://postgres:[Dhananjay@0908]@db.ylppiisvnjueyawrityq.supabase.co:5432/postgres";
     PGconn *conn = PQconnectdb(conninfo);
 
     if (PQstatus(conn) != CONNECTION_OK) {
